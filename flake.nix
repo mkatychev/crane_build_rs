29c117e94c14fa5e1800fb0c6d440bde46cf13ac
Mikhail Katychev <mkatych@gmail.com>
2023-06-15 12:48:22 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "my-package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane = { url = "github:ipetkov/crane"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # libs
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib;
+        craneLib = crane.lib.${system};
+
+        myPackage = craneLib.buildPackage {
+          src = ./.;
+          pname = "my-package";
+
+          buildInputs = [ pkgs.protobuf ];
+          doCheck = false;
+          cargoExtraArgs = "--package my-package";
+        };
+
+      in
+      {
+        # $ nix flake check
+        checks = {
+          inherit myPackage ;
+        };
+
+        packages = { default = myPackage; };
+
+      });
+}
